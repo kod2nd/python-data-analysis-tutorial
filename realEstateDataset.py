03276d1b02ba42b783d8b98a4b0ded1238edd4f8
kod2nd <colinongengkiat@gmail.com>
2018-09-24 17:46:24 +0800

    getting dataset from Quandl

--- realEstateDataset.py
+++ realEstateDataset.py
@@ -1,8 +1,17 @@
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import quandl
 
 load_dotenv('./.env')
-
 API_KEY = os.getenv("API_KEY")
 
-print(API_KEY)
+# df = quandl.get('FMAC/HPI_AK', authtoken=API_KEY)
+# print(df.head())
+
+fifty_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+
+abbriviations = fifty_states[0][1][1:]
+
+for abbriviation in abbriviations:
+    print('FMAC/HPI_{}'.format(abbriviation))
